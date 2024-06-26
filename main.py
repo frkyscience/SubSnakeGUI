0c97132c4305c921a813031d8b6f2d3823bb173e
frkyscience <starfart1@gmail.com>
2024-06-26 06:25:10 +0200

    imprt test

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from colorama import Fore, init
+import requests
+from dns import resolver
+import nmap
+import os
+from os import path
+
+
+
+init(autoreset=True)
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ct,rl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
